b090ab05bfd022d92ed302db2214b89a6f9c986c
lynnwolters <47858242+lynnwolters@users.noreply.github.com>
2022-12-05 22:05:27 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,15 @@
-// JavaScript Document
-console.log("hi");
+// stap 1: zoek de menu-button op en sla die op in een variabele
+let deMenuButton = document.querySelector("header nav:nth-of-type(1) > button");
+let deSluitButton = document.querySelector("header nav:nth-of-type(1) > nav button");
+
+// stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
+deMenuButton.addEventListener("click", toggleMenu);
+deSluitButton.addEventListener("click", toggleMenu);
+
+// stap 3: voeg in de functie een class toe aan de nav
+function toggleMenu() { 
+let deNav = document.querySelector("header nav:nth-of-type(1) > nav");
+  deNav.classList.toggle("open");
+}
+
+
